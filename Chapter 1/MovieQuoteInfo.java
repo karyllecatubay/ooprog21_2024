8b1f8b2adaead24f4eb73c6f417346c15d6cae04
karylle05 <karyllecatubigcatubay@gmail.com>
2024-08-14 20:17:57 +0800

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,12 @@
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+        
+         System.out.printf(
+            "Quote: %s%nMovie: %s%nCharacter: %s%nYear: %d%n",
+            "-Let them hate, just make sure they spell your name right.-",
+            "Suits",
+            "Harvey Specter",
+            2012
+        );
+    }
+}
---
+++ Chapter 1/TableAndChairs.java
@@ -0,0 +1,17 @@
+public class TableAndChairs{
+
+   public static void main(String[] args) {
+   
+   //chairs on the left and right 
+      System.out.println("X                              X");
+      System.out.println("X                              X");
+      System.out.println("X                              X");
+      System.out.println("X          XXXXXXXXXX          X");
+      System.out.println("XXXXXX     X        X     XXXXXX");
+      System.out.println("X    X     X        X     X    X");
+      System.out.println("X    X     X        X     X    X");
+      System.out.println("X    X     X        X     X    X");
+      
+   }
+
+}
