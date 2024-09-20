28fd9314407deb52f14818c2d6e1aede2eeb1d35
karyllecatubay <karyllecatubigcatubay@gmail.com>
2024-09-20 21:20:01 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,15 @@
+public class FirstAddressExercise {
+
+   public static void displayAddress(){
+   
+      System.out.println("Huawei Company");
+      System.out.println("Huawei Base, Bantian, Longgang District");
+      System.out.println("Shenzhen 518129 P.R. China");
+      System.out.println("Company: Huawei Technologies Co., Ltd.");  
+   }
+
+   public static void main(String [] args){
+   
+      displayAddress();
+   }
+}
