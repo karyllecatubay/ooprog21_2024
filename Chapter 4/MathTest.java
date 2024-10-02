c09c4ca02e4820ae75bc9d0e18a86ffb7f59d82d
karyllecatubay <karyllecatubigcatubay@gmail.com>
2024-10-02 21:33:25 +0800

    Add files via upload

---
+++ Chapter 4/MathTest.java
@@ -0,0 +1,22 @@
+public class MathTest{
+     
+     public static void main(String[] args){
+     
+         System.out.println("SQRT: " + Math.sqrt(39));
+         
+         System.out.println("SIN: " + Math.sin(Math.toRadians(270)));
+         System.out.println("COS: " + Math.sin(Math.toRadians(270)));
+         
+         System.out.println("FLOOR: " + Math.floor(23));
+         System.out.println("CEIL: " + Math.ceil(23));
+
+         System.out.println("ROUND: " + Math.round(20.5));
+         
+         System.out.println("MAX: " + Math.max(70,80));
+         System.out.println("MIN: " + Math.min(70,80));
+         
+
+         System.out.println("RANDOM: " + (Math.random()* 100));
+        
+     }
+}
